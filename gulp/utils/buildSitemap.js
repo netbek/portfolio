d8e4ed791397833a8130159d52b3f204f46757ea
Hein Bekker <hein@netbek.co.za>
2023-09-03 23:09:01 +0200

    Fix separator

--- gulp/utils/buildSitemap.js
+++ gulp/utils/buildSitemap.js
@@ -1,5 +1,4 @@
 const fs = require('fs-extra');
-const path = require('path');
 const loadData = require('./loadData');
 
 module.exports = async (dest) => {
@@ -7,12 +6,12 @@ module.exports = async (dest) => {
   const urls = [
     ...Object.entries(data.pages).map(([slug, page]) => {
       if (!page.isFront) {
-        return path.join(data.settings.site.url, slug);
+        return data.settings.site.url + '/' + slug;
       }
     }),
     ...Object.entries(data.projects).map(([slug, page]) => {
       if (!page.isFront) {
-        return path.join(data.settings.site.url, 'work', slug);
+        return data.settings.site.url + '/' + 'work' + '/' + slug;
       }
     })
   ]
